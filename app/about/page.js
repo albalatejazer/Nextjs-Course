00c4af3b6a60f3d6ea5363d4d381157777c85c49
mjazer <albalate.jazer@gmail.com>
2025-02-19 22:18:48 +0800

    2nd day: working with pagen layout

---
+++ app/about/page.js
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <main>
+        <h1>
+          About us is the Nulla cupidatat dolore eu sunt in ut nulla ut ea et ex. Ut
+          quis pariatur ad incididunt esse deserunt fugiat. Aute esse id dolor ea
+          minim ex sint do magna duis ex consequat. Non reprehenderit adipisicing
+          non aliqua sit. Dolore labore aliquip dolor culpa adipisicing et et.
+        </h1>
+    </main>
+  );
+}
